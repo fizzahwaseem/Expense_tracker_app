f95dcd6560b194cbd387e0f5b1b8dfac10810c71
fizzahwaseem <50530599+fizzahwaseem@users.noreply.github.com>
2020-08-10 19:16:24 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+import { GlobalProvider } from './context/GlobalState';
+import './App.css';
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className='container'>
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
---
+++ src/components/TransactionList.js
@@ -0,0 +1,18 @@
+import React, {useContext} from 'react';
+import { GlobalContext } from '../context/GlobalState'; 
+
+export const TransactionList = () => {
+    const context = useContext(GlobalContext);
+    console.log(context);
+
+    return (
+        <>
+        <h3>History</h3>
+        <ul className="list">
+            <li className='minus'>
+                cash <span>$-400</span><button className="delete-btn">x</button>
+            </li>
+        </ul>
+        </>
+    )
+}
