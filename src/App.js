e3ad398e0ea19985fbe865eb8a55d40c72342c45
fizzahwaseem <50530599+fizzahwaseem@users.noreply.github.com>
2020-08-10 19:26:16 +0500

    updated App.js and GlobaState.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Header } from './components/Header';
 import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
+
 import { GlobalProvider } from './context/GlobalState';
 import './App.css';
 
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -12,6 +12,9 @@ const initialState = {
 }
 
 //Create context
+export const GlobalContext = createContext(initialState);
+
+//Provider component
 export const GlobalProvider = ({children}) => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
     return(<GlobalContext.Provider value={{
