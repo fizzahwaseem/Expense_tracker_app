47833a1ee88cf8d02c2a2ce1fda7c02bea5efe8d
fizzahwaseem <50530599+fizzahwaseem@users.noreply.github.com>
2020-08-12 21:44:48 +0500

    updated GlobalState and AddTransaction

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -5,16 +5,19 @@ import { GlobalContext } from '../context/GlobalState';
 export const AddTransaction = () => {
     const [text, setText] = useState('');
     const [amount, setAmount] = useState(0);
-    const{ addTransaction } = useContext(GlobalContext);
-    const onSubmit = e => {
-        e.preventDefault();
-        const newTransaction = {
-            id: Math.floor(Math.random() * 100000000),
-            text,
-            amount: +amount
-        }
-        addTransaction(newTransaction);
-    }
+  
+    const { addTransaction } = useContext(GlobalContext);
+  
+    const onSubmit = (e) => {
+      e.preventDefault();
+  
+      const newTransaction = {
+        id: Math.floor(Math.random() * 100000000),
+        text,
+        amount: +amount,
+      };
+      addTransaction(newTransaction);
+    };
 
     return (
         <div>
